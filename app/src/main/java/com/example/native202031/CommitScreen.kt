fa315e8c1f10d7692e34fd7165c3a4800749481f
yono.ap <yono.ap@icloud.com>
2021-08-12 14:03:27 +0900

    repo commits.

--- app/src/main/java/com/example/native202031/CommitScreen.kt
+++ app/src/main/java/com/example/native202031/CommitScreen.kt
@@ -1,8 +1,8 @@
 package com.example.native202031
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.native202031.ui.theme.Native202031Theme
 
@@ -18,7 +19,8 @@ import com.example.native202031.ui.theme.Native202031Theme
 fun CommitScreen(viewModel: CommitViewModel = hiltViewModel()) {
     val userName by viewModel.userName.collectAsState()
     val repo by viewModel.repo.collectAsState()
-    CommitContent(userName = userName, repo = repo)
+    val commits by viewModel.commits.collectAsState()
+    CommitContent(userName = userName, repo = repo, commits = commits)
 
     val progress by viewModel.progress.collectAsState()
 
@@ -38,7 +40,7 @@ fun CommitScreen(viewModel: CommitViewModel = hiltViewModel()) {
 }
 
 @Composable
-fun CommitContent(userName: String, repo: String) {
+fun CommitContent(userName: String, repo: String, commits: List<CommitItem>) {
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -46,7 +48,21 @@ fun CommitContent(userName: String, repo: String) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = userName)
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = repo)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(
+            contentPadding = PaddingValues(0.dp, 0.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(commits) { commit ->
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = commit.message, fontSize = 16.sp)
+                    Text(text = commit.name, fontSize = 10.sp)
+                    Text(text = commit.date, fontSize = 10.sp)
+                }
+            }
+        }
     }
 }
 
@@ -54,6 +70,10 @@ fun CommitContent(userName: String, repo: String) {
 @Composable
 fun CommitPreview() {
     Native202031Theme {
-        CommitContent("user name", "repo")
+        val list = listOf(
+            CommitItem(message = "message1", name = "name1", date = "date1"),
+            CommitItem(message = "message2", name = "name2", date = "date2")
+        )
+        CommitContent("user name", "repo", list)
     }
 }
--- app/src/main/java/com/example/native202031/CommitViewModel.kt
+++ app/src/main/java/com/example/native202031/CommitViewModel.kt
@@ -3,11 +3,14 @@ package com.example.native202031
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.example.native202031.database.AppCache
+import com.example.native202031.network.CommitModel
+import com.example.native202031.network.ServerAPI
 import com.example.native202031.preference.AppPrefs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.serialization.builtins.ListSerializer
 import java.util.*
 import javax.inject.Inject
 
@@ -15,7 +18,8 @@ import javax.inject.Inject
 class CommitViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val appPrefs: AppPrefs,
-    private val appCache: AppCache
+    private val appCache: AppCache,
+    private val serverAPI: ServerAPI
 ) : BaseViewModel() {
 
     private val _userName = MutableStateFlow(Date().toBestString())
@@ -23,6 +27,9 @@ class CommitViewModel @Inject constructor(
 
     private val _repo = MutableStateFlow("")
     val repo: StateFlow<String> = _repo
+
+    private val _commits = MutableStateFlow(listOf<CommitItem>())
+    val commits: StateFlow<List<CommitItem>> = _commits
 
     init {
         logger.info("init")
@@ -35,15 +42,19 @@ class CommitViewModel @Inject constructor(
                 savedStateHandle.get<String>("repo")?.let {
                     _repo.value = it
                 }
+                val commits = serverAPI.getDecode(
+                    serverAPI.getCommitsUrl(userName.value, repo.value),
+                    ListSerializer(CommitModel.serializer())
+                )
+                commits.map { CommitItem.fromCommitModel(it) }
+            }.onSuccess {
+                _commits.value = it
             }.onFailure {
                 logger.error("init", it)
+                showDialog(it.message, it.javaClass.simpleName)
             }.also {
                 hideProgress()
             }
         }
-    }
-
-    fun test() {
-        logger.debug("test")
     }
 }
